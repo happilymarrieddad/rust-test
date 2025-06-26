ec4a6b991528877d2a6e5b9edd4c8d89050a8671
Nick Kotenberg <happilymarrieddad@gmail.com>
2025-06-26 10:14:40 -0600

    updated

--- old-world-builder-rust/src/api/mod.rs
+++ old-world-builder-rust/src/api/mod.rs
@@ -1,8 +1,1 @@
-use actix_web::{get, post, web, App, HttpResponse, Responder};
-
-pub fn init(app: App) -> App {
-    app
-        .service(hello)
-        .service(echo)
-        .route("/hey", web::get().to(manual_hello))
-}
+pub mod v1; 
--- old-world-builder-rust/src/main.rs
+++ old-world-builder-rust/src/main.rs
@@ -1,13 +1,19 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, web};
 
 mod api;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
-        api::init(App::new())
+        App::new()
+            // .route("/1", web::service(
+            //     web::route("/",web::get().to(api::v1::handlers::manual_hello))
+            // ))
+            .service(api::v1::handlers::hello)
+            .service(api::v1::handlers::echo)
+            .route("/hey", web::get().to(api::v1::handlers::manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
